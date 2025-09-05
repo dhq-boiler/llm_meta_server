a1b61ccd00431a5a3c41f84f430491d1e31b5b5c
dhq_boiler <dhq_boiler@live.jp>
2025-09-05 16:27:22 +0900

    Add methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,34 @@ class User < ApplicationRecord
       user.google_id = auth.uid
     end
   end
+
+  # 認証プロバイダーを判定するメソッド
+  def authentication_provider
+    return :google_oauth2 if google_id.present?
+    # 将来的に他のIdPを追加する場合はここに条件を追加
+    # return :azure_oauth2 if azure_id.present?
+    # return :github if github_id.present?
+
+    :unknown
+  end
+
+  # Google SSOユーザーかどうかを判定
+  def google_sso_user?
+    authentication_provider == :google_oauth2
+  end
+
+  # SSO（シングルサインオン）ユーザーかどうかを判定
+  def sso_user?
+    authentication_provider != :unknown
+  end
+
+  # 認証プロバイダーの表示名を取得
+  def authentication_provider_name
+    case authentication_provider
+    when :google_oauth2
+      'Google'
+    else
+      '不明'
+    end
+  end
 end
