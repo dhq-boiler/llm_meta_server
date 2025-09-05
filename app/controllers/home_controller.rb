276647d5d5e30c113530bbb1fef7eb516bad2528
dhq_boiler <dhq_boiler@live.jp>
2025-09-05 15:30:09 +0900

    Add home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
     if user_signed_in?
-      # Logged-in users will be redirected to their profile page.
-      # redirect_to user_profile_path
+      redirect_to user_profile_path
+    else
+      # 未認証ユーザー向けのランディングページ
     end
   end
 end
